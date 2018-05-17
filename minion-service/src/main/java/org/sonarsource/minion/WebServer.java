71dc6b539f9a64945aae9befd6314dbcb8efe583
Julien Lancelot <julien.lancelot@sonarsource.com>
2018-05-17 14:00:28 +0200

    Add basic web server

---
+++ minion-service/src/main/java/org/sonarsource/minion/WebServer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2018-2018 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+package org.sonarsource.minion;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+public class WebServer {
+
+  private static final int PORT = 9001;
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(WebServer.class);
+
+  public void start() {
+    port(PORT);
+    LOGGER.info("Listening on port {}", PORT);
+
+    post("/analyze", (request, response) -> {
+      String message = request.body();
+      return new Analyzer().analyze(message);
+    });
+  }
+
+  public static void main(String[] args) {
+    new WebServer().start();
+  }
+}
