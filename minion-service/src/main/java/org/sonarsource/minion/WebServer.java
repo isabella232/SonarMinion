6c536163898878bdc2965e6ab3c53c0b8acefd58
Julien Lancelot <julien.lancelot@sonarsource.com>
2018-05-17 17:22:40 +0200

    Allow customization of web server port

--- minion-service/src/main/java/org/sonarsource/minion/WebServer.java
+++ minion-service/src/main/java/org/sonarsource/minion/WebServer.java
@@ -14,15 +14,15 @@ import static spark.Spark.post;
 
 public class WebServer {
 
-  private static final int PORT = 9001;
+  private static final int DEFAULT_PORT = 9001;
 
   private static final Logger LOGGER = LoggerFactory.getLogger(WebServer.class);
 
   private final Analyzer analyzer = new Analyzer();
 
-  public void start() {
-    port(PORT);
-    LOGGER.info("Listening on port {}", PORT);
+  public void start(int port) {
+    port(port);
+    LOGGER.info("Listening on port {}", port);
 
     post("/analyze", (request, response) -> {
       String message = request.body();
@@ -31,6 +31,11 @@ public class WebServer {
   }
 
   public static void main(String[] args) {
-    new WebServer().start();
+    if (args.length > 0){
+      String port = args[0];
+      new WebServer().start(Integer.parseInt(port));
+      return;
+    }
+    new WebServer().start(DEFAULT_PORT);
   }
 }
