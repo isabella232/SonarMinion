4a45b79b1b675e6f21296513a2b0f74bb074b5c4
Julien Lancelot <julien.lancelot@sonarsource.com>
2018-05-17 14:52:43 +0200

    Read plugins from update center

---
+++ minion-service/src/main/java/org/sonarsource/minion/UpdateCenter.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017-2018 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+package org.sonarsource.minion;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
+public class UpdateCenter {
+
+  private final Properties updateCenterProperties;
+
+  private final Set<String> pluginKeys;
+  private final Set<String> pluginNames;
+
+  public UpdateCenter() {
+    this.updateCenterProperties = loadFile();
+    this.pluginKeys =
+      stream(this.updateCenterProperties.getProperty("plugins").split(","))
+      .collect(Collectors.toSet());
+    this.pluginNames = pluginKeys.stream().map(pluginKey -> updateCenterProperties.getProperty(pluginKey + ".name"))
+      .collect(Collectors.toSet());
+  }
+
+  private Properties loadFile() {
+    try {
+      Properties updateCenterProperties = new Properties();
+      updateCenterProperties.load(getClass().getResourceAsStream("/update-center.properties"));
+      return updateCenterProperties;
+    } catch (IOException e) {
+      throw new IllegalStateException("Can't read update center file");
+    }
+  }
+
+  public Collection<String> findProducts() {
+    return pluginNames;
+  }
+}
---
+++ minion-service/src/test/java/org/sonarsource/minion/UpdateCenterTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2017-2018 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+package org.sonarsource.minion;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UpdateCenterTest {
+
+  private UpdateCenter underTest = new UpdateCenter();
+
+  @Test
+  public void find_products() {
+    assertThat(underTest.findProducts()).contains("SonarCOBOL");
+  }
+}
